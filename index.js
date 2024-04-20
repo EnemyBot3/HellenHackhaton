c0b7450cf588f20d5c9be904afda8ce7e4f14ea0
James <jamespearce815@gmail.com>
2024-04-20 16:48:59 +0100

    Add message about database being turned off

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var con = mysql.createConnection({
-  host: "halle-history.cluster-ctiiwouaw2qm.eu-west-2.rds.amazonaws.com",
+  host: "", // I've turned off the database as turns out it's charging per hour. Sorry I should have said at the end of the day. If you install mysql locally you should be able to access the DB in the same way by using localhost as the host here
   user: "admin",
-  password: process.env.HALLE_DB_PASS
+  password: process.env.HALLE_DB_PASS,
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
